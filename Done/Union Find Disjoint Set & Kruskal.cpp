27d6916d2d4358ddd16f8c3519a32084ceec29ba
iman irandoost <imn.irdst@gmail.com>
2015-08-11 10:52:44 +0430

    Fixing Union Find Distjoin Set Code

--- Done/Union Find Disjoint Set & Kruskal.cpp
+++ Done/Union Find Disjoint Set & Kruskal.cpp
@@ -1,22 +1,46 @@
-1.	Graphic Sequence
-// given a sequence of integers see if it’s a sequence of degrees of graph or not.
-int a[10010]; long long sum,Min;;
+struct DisJointSet{
+	vector<int> par,rnk,cnt; int numOfsets;
 
-int main(){
-	int n;
-	while(cin >> n && n){
-		for(int i=0 ; i<n ; i++) scanf("%d",&a[i]);
-		sort(a,a+n, ::greater<int>() );
-		bool possible=true; sum=0;
-		for(int i=0 ; i<n ; i++){
-			sum+=a[i]; Min=0;
-			for(int j=i+1 ; j<n; j++) Min+=min(a[j],i+1);
-			if(sum>i*(i+1)+Min){
-				possible=false;
-				break;
-			}
-		}
-		if(!possible || sum%2) cout << "Not possible" << endl;
-		else cout << "Possible" << endl;
+	DisJointSet(int n){
+		par.assign(n,-1); rnk.assign(n,0); cnt.assign(n,1); //par==parent
+		numOfsets=n; // if wanna count number of disjoint sets
 	}
-	return 0;}
+
+	int Find(int a){
+		int i=a,j=a,tmp;
+		while(par[i]!=-1){ i=par[i]; }
+		while(par[j]!=-1){ tmp=par[j]; par[j]=i; j=tmp; } //path compression
+		return i;
+	}
+	int Uni(int a, int b){
+		int A=Find(a),B=Find(b);
+		if(A!=B){
+			if(rnk[A]<rnk[B]) swap(A,B); // union using rank
+			if(rnk[A]==rnk[B]) rnk[A]++;
+			par[B]=A;
+			cnt[A]+=cnt[B]; // if we wanna count each set size
+			numOfsets--; // if wanna count number of disjoint sets
+		}
+		return cnt[A]; // if we wanna count each set size
+	}
+};
+
+struct edge{ int u, v, w;
+	edge(int u=0, int v=0, int w=0):u(u), v(v), w(w){};
+	bool operator<(const edge& b) const {
+		if(w == b.w && v == b.w) return u < b.u;
+		if(w == b.w ) return v < b.v; return w < b.w;
+	}
+};
+
+int n, m, bit; vector<edge> e; vi marked;
+
+int Kruskal(){
+	DisJointSet djst(n); marked.clear();
+	sort(e.begin(), e.end()); int ans=0; int j=0;
+	for(int i=0; i<e.size() && j<n-1 ; i++){
+		if(djst.Find(e[i].u) != djst.Find(e[i].v)){
+			djst.Uni(e[i].u, e[i].v); ans+=e[i].w; j++; marked.push_back(i);
+		}
+	}
+	return ans;}
