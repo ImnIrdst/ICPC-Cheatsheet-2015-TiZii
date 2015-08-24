28fd5cd223122a3fda5acfaecc46de71b6fe9fe2
Iman Irandoost <imn.irdst@gmail.com>
2015-08-23 17:24:19 -0700

    Tricks.cpp file added

--- Done/Essential Headers.cpp
+++
@@ -1,5 +0,0 @@
-#include <algorithm>
-#include <cstring>
-#include <cstdio>
-#include <string>
-#include <cmath>
---
+++ Done/Tricks.cpp
@@ -0,0 +1,13 @@
+Essential Headers:
+	#include <cstring>
+	#include <cstdio>
+	#include <cmath>
+Wrong Answer:
+	- getline 
+	- Zero (lower bound test case)
+Time Limit:
+	- Use runtime < 1e6/2
+Compile error:
+	- using abs or fabs without cmath
+	- using memset without cstring
+	- using printf or scanf without cstdio
